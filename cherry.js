61f0ceee010b589d7649722497af7d405262f2e8
falloff626 <falloff.stabler-0d@icloud.com>
2024-08-06 21:16:00 +0500

    Updated testing framework

---
+++ cherry.js
@@ -0,0 +1,24 @@
+class Queue {
+  constructor() {
+    this.items = {};
+    this.frontIndex = 0;
+    this.backIndex = 0;
+  }
+  enqueue(item) {
+    this.items[this.backIndex] = item;
+    this.backIndex++;
+    return item + ' inserted';
+  }
+  dequeue() {
+    const item = this.items[this.frontIndex];
+    delete this.items[this.frontIndex];
+    this.frontIndex++;
+    return item;
+  }
+  peek() {
+    return this.items[this.frontIndex];
+  }
+  get printQueue() {
+    return Object.values(this.items);
+  }
+}
