ab65ae3e2cfd03f2eca82d143434c4febb689860
falloff626 <falloff.stabler-0d@icloud.com>
2024-08-28 07:10:00 +0500

    Fixed issue with file uploads

---
+++ dashboard.js
@@ -0,0 +1,1 @@
+class Person { constructor(name, age) { this.name = name; this.age = age; } sayHello() { console.log(`Hello, my name is ${this.name}`); } } const john = new Person('John', 30); john.sayHello();
